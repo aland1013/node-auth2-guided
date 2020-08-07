ad935f9c5497892ac764968125f534feaada6ff5
aland1013 <adam.land@gmail.com>
2020-08-06 23:07:30 -0400

    finished

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,13 @@
-const bcryptjs = require("bcryptjs");
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const router = require("express").Router();
+const secrets = require('../config/secrets');
+const router = require('express').Router();
 
-const Users = require("../users/users-model.js");
-const { isValid } = require("../users/users-service.js");
+const Users = require('../users/users-model.js');
+const { isValid } = require('../users/users-service.js');
 
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   const credentials = req.body;
 
   if (isValid(credentials)) {
@@ -18,20 +20,21 @@ router.post("/register", (req, res) => {
 
     // save the user to the database
     Users.add(credentials)
-      .then(user => {
+      .then((user) => {
         res.status(201).json({ data: user });
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        'please provide username and password and the password shoud be alphanumeric'
     });
   }
 });
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   if (isValid(req.body)) {
@@ -39,19 +42,37 @@ router.post("/login", (req, res) => {
       .then(([user]) => {
         // compare the password the hash stored in the database
         if (user && bcryptjs.compareSync(password, user.password)) {
-          res.status(200).json({ message: "Welcome to our API" });
+          const token = generateToken(user);
+          res.status(200).json({ message: 'Welcome to our API', token });
         } else {
-          res.status(401).json({ message: "Invalid credentials" });
+          res.status(401).json({ message: 'Invalid credentials' });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         res.status(500).json({ message: error.message });
       });
   } else {
     res.status(400).json({
-      message: "please provide username and password and the password shoud be alphanumeric",
+      message:
+        'please provide username and password and the password shoud be alphanumeric'
     });
   }
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role
+  };
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  const secret = secrets.jwtSecret;
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -1,14 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const Users = require("./users-model.js");
-const restricted = require("../auth/restricted-middleware.js");
+const Users = require('./users-model.js');
+const restricted = require('../auth/restricted-middleware.js');
+const checkRole = require('../auth/check-role-middleware');
 
-router.get("/", restricted, (req, res) => {
+router.get('/', restricted, checkRole(1), (req, res) => {
   Users.find()
-    .then(users => {
+    .then((users) => {
       res.status(200).json(users);
     })
-    .catch(err => res.send(err));
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
